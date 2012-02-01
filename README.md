21ff95295565b93e52b50a1a9681c032235c648a
David Barker <kzar@kzar.co.uk>
2012-02-01 18:20:54 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+bitratchet-js
+=============
+
+This library is work in progress, don't attempt to use it yet. - Dave
+---------------------------------------------------------------------
+
+A Javascript library for processing binary data, similar to BinData for Ruby and taking ideas from my earlier (much more limited) PHP library.
+
+Makes use of the new ArrayBuffer Javascript feature and allows for simple (yet flexible) parsing and "unparsing" of binary data.
+Features include:
+
+ - Avoid duplicate code, you only have to specify records once as with BinData.
+ - Deal transparently with bit (as opposed to byte) based fields, shifting is taken care of.
+ - Handle dynamic fields that can vary in length and structure.
+ - Build your own flexible primtives that can extend the library to your problem domain.
+
+Developed by Dave Barker to aid parsing of a implementing a commerical telematics messaging protocol in Javascript.
+
+Usage
+-----
+
+Make sure you're importing the bitratchet library:
+
+      <script src="js/bit-ratchet.js" type="text/javascript"></script>
+
+Primitives
+----------
+
+Primitives are used to parse individual fields, they are functions return a primitive object based on the options passed.
+The primitive object that's returned must follow these rules:
+
+ - It must contain a `parse` field containing a function that expects data in a ArrayBuffer and returns the parsed information.
+ - It must contain an `unparse` field containing a function that accepts the parsed information and returns an ArrayBuffer with the unparsed data.
+ - It must contain a length field containing a number specifying - in bits - how large the primtive is. If the primitive is of a static length this should be set immediately, if the primtive is of dynamic length it should be initially set to 0 and then adjusted by the parse and unparse functions.
+ - If the primtive's length is not divisible by 8 it must ignore the remaining bits inside the last byte of the ArrayBuffer.
+ - If the primitive is created with invalid options, or used with invalid data an exception should be thrown.
+
+Included primitives:
+
+ - `number` can be used for most types of numbers. It handles signed / unsigned, scaled / integer and rounding.
+      Expects an options object that can contain the following options:
+      {
+        length : Length of the number in bits, this is required.
+        signed : Is the number signed? (Default is false.)
+        precision : Decimal place precision to round to. (Default is no rounding.)
+        scale_range : If the number is scaled to a range provide the range. (The same as providing a custom_scale of Math.pow(2, options.length) / options.scale_range)
+        custom_scale : If the number is scaled inefficiently you can directly provide the scale.
+      }
+ - `record`
+ - `flags`
+ - `dynamic`
+ - `hex`
+
+Records
+-------
+
+License
+-------
+Copyright Dave Barker 2012
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ bit-ratchet.js
@@ -0,0 +1,291 @@
+// BitRatchet - Javascript binary data processing library similar to BinData
+// Released under MIT licence, Copyright Dave Barker 2012
+
+var BitRatchet;
+if (!BitRatchet) {
+    BitRatchet = {};
+}
+
+(function () {
+    "use strict";
+
+    if (typeof BitRatchet.record !== 'function') {
+        BitRatchet.record = function record(structure) {
+            function map_fields(f) {
+                for (var field in structure) {
+                    if (structure.hasOwnProperty(field)) {
+                        f(field, structure[field]);
+                    }
+                }
+            }
+            return {
+                parse : function (data) {
+                    var result = {}, position = 0;
+                    map_fields(function (k, v) {
+                        var byte_offset, bit_offset, spare_bits, shifted_data;
+                        if (position % 8 === 0) {
+                            // If our position falls on a byte just parse the field
+                            if (v.length) {
+                                // If field has set length just pass it required data
+                                result[k] = v.parse(data.subarray(position / 8, position / 8 + Math.ceil(v.length / 8)))
+                            } else {
+                                // Otherwise pass it all remaining data
+                                result[k] = v.parse(data.subarray(position / 8));
+                            }
+                        } else {
+                            // Our position falls over bytes so shift data along first
+                            byte_offset = Math.floor(position / 8);
+                            bit_offset = position % 8;
+                            if (v.length) {
+                                // Field has set length so just shift required data
+                                shifted_data = new Uint8Array(new ArrayBuffer(Math.ceil(v.length / 8)));
+                            } else {
+                                // Field has dynamic length so shift all remaining data
+                                shifted_data = new Uint8Array(new ArrayBuffer(data.length - byte_offset));
+                            }
+                            spare_bits = 0;
+                            for (i = 0; i < shifted_data.length; i += 1) {
+                                shifted_data[i] = (data[i + byte_offset] >> bit_offset) | spare_bits;
+                                spare_bits = data[i + byte_offset] & bit_offset;
+                            }
+                            // Now we can parse it
+                            result[k] = v.parse(shifted_data);
+                        }
+                        position += v.length;
+                    });
+                    this.length = position;
+                    return result;
+                },
+                unparse : function (data) {
+                    var results = [], assembled_data, byte_position, bit_offset;
+                    // First parse each part collecting the result and it's length
+                    this.length = 0;
+                    map_fields(function(k, v) {
+                        results.concat({ value : v.unparse(data[k]), length : v.length });
+                        this.length += v.length;
+                    });
+                    // Now put all those results into an ArrayBuffer and return
+                    assembled_data = new Uint8Array(new ArrayBuffer(Math.ceil(this.length / 8)));
+                    bit_offset = 0;
+                    byte_position = 0;
+                    for (i = 0; i <  results.length; i += 1) {
+                        for (j = 0; j < results[i].value.length; j += 1) {
+                            if (bit_offest === 0) {
+                                // No bit offset so copy byte over straight
+                                assembled_data[byte_position] = results[i].value[j];
+                            } else {
+                                // Take account of bit offset when copying byte over
+                                assembled_data[byte_position] = assembled_data[byte_position] | results[i].value[j] >> bit_offset;
+                                if (byte_position + 1 < assembled_data.length) {
+                                    assembled_data[byte_position + 1] = results[i].value[j] & bit_offset;
+                                }
+                            }
+                            byte_position += 1;
+                        }
+                        bit_offset = (bit_offest + results[i].length) % 8;
+                    }
+                    return assembled_data;
+                },
+                length: 0
+            };
+        }
+    }
+
+    if (typeof BitRatchet.number !== 'function') {
+        (function () {
+            // Helper functions for working with numbers
+            function binary_to_number(bytes, signed, bit_count) {
+                var i, byte_count, number;
+                byte_count = Math.ceil(bit_count / 8);
+                for (i = 0; i < byte_count; i += 1) {
+                    if (i === 0) {
+                        // For last byte mask any spare bits
+                        number = bytes[byte_count - 1] & Math.pow(2, 8 - bit_count % 8);
+                    } else {
+                        // For other bytes add 'em on
+                        number += bytes[byte_count - i - 1] * Math.pow(2, i * 8);
+                    }
+                }
+                return signed ? number >> 0 : number >>> 0;
+            }
+            function number_to_binary(number, bit_count) {
+                var i = 0, bytes = new Uint8Array(new ArrayBuffer(Math.ceil(bit_count / 8)));
+                while (i < bytes.length) {
+                    if (i === 0) {
+                        // For last byte mask any spare bits
+                        bytes[bytes.length - 1] = number & Math.pow(2, 8 - bit_count % 8);
+                    } else {
+                        // For others add 'em in
+                        bytes[bytes.length - i - 1] = number / Math.pow(2, i * 8) & 0xff;
+                    }
+                    i += 1;
+                }
+                return bytes;
+            }
+            function round_number(number, precision) {
+                if (precision === undefined) {
+                    return number;
+                } else {
+                    return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision);
+                }
+            }
+            function scale(options) {
+                return (options.custom_scale ||
+                        (options.scale_range && options.scale_range /
+                         Math.pow(2, options.length)) || 1);
+            }
+            // Actual number function
+            BitRatchet.number = function number(options) {
+                return {
+                    parse : function (data) {
+                        return round_number(binary_to_number(data, options.length, options.signed) * scale(options), options.precision);
+                    },
+                    unparse : function (data) {
+                        return number_to_binary(Math.round(data / scale(options)) >>> 0, options.length);
+                    },
+                    length: options.length
+                };
+            }
+        })();
+    }
+
+    if (typeof BitRatchet.lookup !== 'function') {
+        BitRatchet.lookup = function lookup(options) {
+            return {
+                parse : function (data) {
+                    var index = options.type.parse(data);
+                    this.length = options.type.length;
+                    return index < options.table.length ? options.table[index] : options.missing;
+                },
+                unparse : function (data) {
+                    var i, result;
+                    for (var i = 0; i < options.table.length; i += 1) {
+                        if (lookup[i] === data) {
+                            result = options.type.unparse(i);
+                            this.length = options.type.length;
+                            return result;
+                        }
+                    }
+                    return options.missing;
+                },
+                // FIXME - length can't be guarneteed when unparsing if lookup key is missing
+                // and options.type has variable length. (Unlikely in fairness)
+                length : options.type.length
+            };
+        }
+    }
+
+    if (typeof BitRatchet.flags !== 'function') {
+        BitRatchet.flags = function flags(options) {
+            return {
+                parse : function (data) {
+                    var i, results = {};
+                    // Next assemble the result
+                    for (i = 0; i < options.length; i += 1) {
+                        if (options.flags[i]) {
+                            // Select correct byte, then bit and use it to add result value
+                            results[options.flags[i]] = options.values[(data[Math.floor(i / 8)] >> (i % 8)) & 0xf];
+                        }
+                    }
+                    return results
+                },
+                unparse : function (data) {
+                    var i, current_byte, bytes = new Uint8Array(new ArrayBuffer(Math.ceil(options.length / 8)));
+                    // Work through flags ORing their values onto relevant byte
+                    for (i = 0; i < options.flags.length; i += 1) {
+                        current_byte = Math.floor(i / 8);
+                        bytes[current_byte] = bytes[current_byte] | (options.values[data[options.flags[i]]] << (i % 8));
+                    }
+                    return bytes;
+                },
+                length : options.length
+            }
+        }
+    }
+
+    if (typeof BitRatchet.dynamic !== 'function') {
+        BitRatchet.dynamic = function dynamic(f) {
+            return {
+                parse : function (data) {
+                    var field = f(),
+                        result = f.parse(data);
+                    this.length = field.length;
+                    return result;
+                },
+                unparse : function (data) {
+                    var field = f(),
+                        result = f.unparse(data);
+                    this.length = field.length;
+                    return result;
+                },
+                length: 0
+            };
+        }
+    }
+
+    if (typeof BitRatchet.hex !== 'function') {
+        BitRatchet.hex = function hex(options) {
+            // Make sure we've been given a valid length
+            if (options.length % 4 !== 0) {
+                throw "Invalid length, must be divisible by 4.";
+            }
+            // If so return the hex object
+            return {
+                parse : function (data) {
+                    var i, hex = '';
+                    // Make sure we've been given an acceptable amount of data
+                    if (data.length * 8 > options.length + 4 ||
+                        data.length * 8 < options.length) {
+                        throw "Wrong amount of data given to parse to hex";
+                    }
+                    // Parse to hex
+                    for (i = 0; i < data.length; i += 1) {
+                        // Pad to byte unless we're on last nibble
+                        if (data[i] < 0x10 && i * 8 < options.length - 8) {
+                            hex += "0";
+                        }
+                        // Either way add the nibble / byte
+                        hex += data[i].toString(16);
+                    }
+                    return hex;
+                },
+                unparse : function (data) {
+                    if (!/^[0-9a-fA-F]+$/.test(data)) {
+                        throw "Invalid hex, can't unparse."
+                    }
+                    var i, bytes = new Uint8Array(new ArrayBuffer(Math.ceil(options.length / 8)));
+                    // Chunk hex
+                    data = data.match(/.{1,2}/g);
+                    // Convert to byte array
+                    for (i = 0; i < bytes.length; i += 1) {
+                        bytes[i] = parseInt(data[i], 16);
+                    }
+                    return bytes;
+                },
+                length : options.length
+            }
+        };
+    }
+})();
+
+
+// FIXME - scrap?
+    function reverse_lookup(object, value) {
+        var key;
+        for (key in object) {
+            if (object.hasOwnProperty(key) && object[key] === value) {
+                return parseInt(key, 10);
+            }
+        }
+    }
+
+
+    function arraybuffer_to_array(arraybuffer) {
+        var i, a = [];
+        for (i = 0; i < arraybuffer.length; i += 1) {
+            a[i] = arraybuffer[i];
+        }
+        return a;
+    }
+
+
